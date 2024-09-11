acebbf352913b68b940692cad692a191d6901538
powerpjvip <154010091+powerpjvip@users.noreply.github.com>
2024-09-11 18:36:31 +0530

    Update leech_utils.py

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -253,7 +253,7 @@ async def format_filename(file_, user_id, dirpath=None, isMirror=False):
     remname = config_dict[f'{ctag}_FILENAME_REMNAME'] if (val:=user_dict.get(f'{ftag}remname', '')) == '' else val
     suffix = config_dict[f'{ctag}_FILENAME_SUFFIX'] if (val:=user_dict.get(f'{ftag}suffix', '')) == '' else val
     lcaption = config_dict['LEECH_FILENAME_CAPTION'] if (val:=user_dict.get('lcaption', '')) == '' else val
-    count = 0
+    #count = 0
     prefile_ = file_
     file_ = re_sub(r'www\S+', '', file_)
         
