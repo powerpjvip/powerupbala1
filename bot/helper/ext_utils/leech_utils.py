652bd46cd65fe3a00949f3e61139d9e14abb7a25
powerpjvip <154010091+powerpjvip@users.noreply.github.com>
2024-09-11 18:59:26 +0530

    Update leech_utils.py

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -273,14 +273,14 @@ async def format_filename(file_, user_id, dirpath=None, isMirror=False):
                 __newFileName = re_sub(args[0], '', __newFileName)
         file_ = __newFileName + ospath.splitext(file_)[1]
         LOGGER.info(f"New Remname : {file_}")
-
+    count += 1
     nfile_ = file_
     if prefix:
         #count += 1
         nfile_ = prefix.replace('\s', ' ') + file_
         prefix = re_sub(r'<.*?>', '', prefix).replace('\s', ' ')
         if not file_.startswith(prefix):
-            file_ = f"{prefix}{file_}"
+            file_ = f"{str(count).zfill(3)}.{prefix}{file_}"
 
     if suffix and not isMirror:
         suffix = suffix.replace('\s', ' ')
@@ -290,7 +290,7 @@ async def format_filename(file_, user_id, dirpath=None, isMirror=False):
         _extIn = 1 + len(fileDict[-1])
         _extOutName = '.'.join(
             fileDict[:-1]).replace('.', ' ').replace('-', ' ')
-        _newExtFileName = f"{_extOutName}{suffix}.{fileDict[-1]}"
+        _newExtFileName = f"{str(count).zfill(3)}.{_extOutName}{suffix}.{fileDict[-1]}"
         if len(_extOutName) > (64 - (sufLen + _extIn)):
             _newExtFileName = (
                 _extOutName[: 64 - (sufLen + _extIn)]
@@ -299,12 +299,12 @@ async def format_filename(file_, user_id, dirpath=None, isMirror=False):
         file_ = _newExtFileName
     elif suffix:
         suffix = suffix.replace('\s', ' ')
-        file_ = f"{ospath.splitext(file_)[0]}{suffix}{ospath.splitext(file_)[1]}" if '.' in file_ else f"{file_}{suffix}"
+        file_ = f"{ospath.splitext(file_)[0]}{suffix}{ospath.splitext(file_)[1]}" if '.' in file_ else f"{str(count).zfill(3)}.{file_}{suffix}"
 
 
     cap_mono =  f"<{config_dict['CAP_FONT']}>{nfile_}</{config_dict['CAP_FONT']}>" if config_dict['CAP_FONT'] else nfile_
     if lcaption and dirpath and not isMirror:
-        count += 1
+        
         def lowerVars(match):
             return f"{{{match.group(1).lower()}}}"
 
